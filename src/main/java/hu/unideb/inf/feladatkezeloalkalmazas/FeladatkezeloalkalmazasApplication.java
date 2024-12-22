d387e653e3ba7ea5e96563fc80e070732d3ac71d
Joó Szebasztián <jooszebi26@gmail.com>
2024-12-22 21:58:42 +0100

    basic maven2

---
+++ src/main/java/hu/unideb/inf/feladatkezeloalkalmazas/FeladatkezeloalkalmazasApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.feladatkezeloalkalmazas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeladatkezeloalkalmazasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeladatkezeloalkalmazasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/unideb/inf/feladatkezeloalkalmazas/FeladatkezeloalkalmazasApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.feladatkezeloalkalmazas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeladatkezeloalkalmazasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
